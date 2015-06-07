703a0fd2b9fc67c716fec031e0ea5f50ac2cee90
Vincent Privat <vincent.privat@gmail.com>
2015-06-07 17:25:19 +0200

    wrong copy/paste

--- src/org/openstreetmap/josm/plugins/areaselector/AreaSelectorPlugin.java
+++ src/org/openstreetmap/josm/plugins/areaselector/AreaSelectorPlugin.java
@@ -33,8 +33,8 @@ public class AreaSelectorPlugin extends Plugin{
 
         LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
         LoggerConfig config = ctx.getConfiguration().getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
-        config.addAppender(console, Level.DEBUG, null);
-        config.setLevel(Level.DEBUG);
+        config.addAppender(console, Level.INFO, null);
+        config.setLevel(Level.INFO);
         ctx.updateLoggers();
         
         areaSelectorAction=new AreaSelectorAction(Main.map);
